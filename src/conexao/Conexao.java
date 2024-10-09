0c46c2f3cf54ac53ec01bcb873e3b1c374f4da04
TorresmoTTV <grand_turismo8@hotmail.com>
2024-10-09 09:55:57 -0300

    first commit

---
+++ src/conexao/Conexao.java
@@ -0,0 +1,22 @@
+package conexao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Conexao {
+    private static String host = "jdbc:mysql://localhost:3306/pi";
+    private static String user = "root";
+    private static String pass = "";
+
+    // Conexao com o banco
+    public static Connection getConexao() throws SQLException {
+        Connection c = null;
+        try {
+            c = DriverManager.getConnection(host, user, pass);
+        } catch (SQLException e) {
+            throw new SQLException("Erro ao conectar!\n" + e.getMessage());
+        }
+        return c;
+    }
+}
